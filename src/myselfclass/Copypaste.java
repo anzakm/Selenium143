6ee9c5039167c471e805ea4f218d4e96dcf7c559
User <User@DESKTOP-KSBVD41>
2019-09-30 17:45:03 -0700

    i T COMMITTED

---
+++ src/myselfclass/Copypaste.java
@@ -0,0 +1,15 @@
+package myselfclass;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Copypaste {
+
+	public static void main(String[] args) {
+		
+WebDriver driver=new FirefoxDriver();
+driver.get("https://www.facebook.com/"
+		+ "");
+	}
+
+}
---
+++ src/myselfclass/Gsmarena.java
@@ -0,0 +1,17 @@
+package myselfclass;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Gsmarena {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver= new FirefoxDriver();
+		driver.get("https://www.gsmarena.com/");
+		
+		
+		
+	}
+
+}
